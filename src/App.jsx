15d140eab34f27079bb01c722595ba03515d771f
‘Katie <ktschr11@gmail.com>
2022-01-05 15:32:47 -0800

    add services

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
 import Home from "./views/Home/Home.jsx";
+import Auth from "./views/Auth/Auth.jsx";
 
 export default function App() {
   return (
         <Router>
           <Switch>
-            <Route>
+            <Route exact path='/'>
               <Home />
+            </Route>
+            <Route path='/login'>
+              <Auth />
+            </Route>
+            <Route path='/register'>
+              <Auth />
             </Route>
           </Switch>
         </Router>
---
+++ src/views/Auth/Auth.jsx
@@ -0,0 +1,8 @@
+
+export default function Auth() {
+  return (
+    <div>
+      <p>BB, you tryin to get authorized?</p>
+    </div>
+  )
+}
